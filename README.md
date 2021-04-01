7031d21ab80760af6d434ad124dfd45e41b36fcb
Taehwan <thkwag@gmail.com>
2021-04-01 16:44:56 +0900

    change damonizer version number

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Install
 ```
-pip install https://github.com/thkwag/python-simple-daemonizer/archive/v0.1.0.tar.gz
+pip install https://github.com/thkwag/python-simple-daemonizer/archive/v0.1.1.tar.gz
 ```
 
 ## Uninstall
--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'simple-daemonizer',
-    version          = '0.1.0',
+    version          = '0.1.1',
     description      = 'Python Simple Daemonizer',
     author           = 'Taehwan Kwag',
     author_email     = 'thkwag@gmail.com',
     license          = 'MIT',
     url              = 'https://github.com/thkwag/python-simple-daemonizer',
-    download_url     = 'https://github.com/thkwag/python-simple-daemonizer/archive/v0.1.0.tar.gz',
+    download_url     = 'https://github.com/thkwag/python-simple-daemonizer/archive/v0.1.1.tar.gz',
     install_requires = [
-        'python-daemon==2.3.0'
+        'python-daemon==2.2.4'
     ],
     packages         = find_packages(exclude = ['docs', 'tests*']),
     keywords         = ['daemonizer, daemon'],
